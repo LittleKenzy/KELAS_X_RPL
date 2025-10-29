b844dff9c6e3612874a65f17e7e660da4ead302b
Littlekenzy <152693956+LittleKenzy@users.noreply.github.com>
2025-10-29 20:28:23 +0700

    update

--- KELAS-X-RPL/VIDEO/Php/EPS_1-52/restoran/kategori/select.php
+++ KELAS-X-RPL/VIDEO/Php/EPS_1-52/restoran/kategori/select.php
@@ -1,36 +1,63 @@
-<?php
+<div style="margin: auto; width: 200px; padding: 10px;">
+    <?php
 
-require_once '../function.php';
+    require_once '../function.php';
 
-$sql = "SELECT * FROM tblkategori";
+    $sql = "SELECT idkategori FROM tblkategori";
+    $result = mysqli_query($koneksi, $sql);
+    $jumlah_data = mysqli_num_rows($result);
 
-$result = mysqli_query($koneksi, $sql);
+    $banyak = 3;
 
-// var_dump($result);
-$jumlah = mysqli_num_rows($result);
+    $halaman = ceil($jumlah_data / $banyak);
 
-// echo "<br>";
+    for ($i = 1; $i < $halaman; $i++) {
+        echo '<a href="?p=' . $i . '">' . $i . '</a>';
+
+        echo '&nbsp;&nbsp;&nbsp;';
+    }
+
+    echo '<br><br>';
+
+    if (isset($_GET['p'])) {
+        $p=$_GET['p'];
+        // echo "Halaman: " . $p . "<br>";
+        $mulai = ($p * $banyak) - $banyak;
+    } else {
+        $mulai = 0;
+    }
+
+    $sql = "SELECT * FROM tblkategori LIMIT $mulai, $banyak";
+
+    $result = mysqli_query($koneksi, $sql);
+
+    // var_dump($result);
+    $jumlah = mysqli_num_rows($result);
+
+    // echo "<br>";
 // echo "<pre>";
-
-// echo "Jumlah data: " . $jumlah . "<br>";
-
-echo '<table border="1">
+    
+    // echo "Jumlah data: " . $jumlah . "<br>";
+    
+    echo '<table border="1">
     <tr>
         <th>No</th>
         <th>Kategori</th>
     </tr>';
 
-$no = 1;
+    $no = $mulai + 1;
 
-if ($jumlah > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo '<tr>';
-        echo '<td>' . $no++ . '</td>';
-        echo '<td>' . $row['kategori'] . '</td>';
-        echo '</tr>';
+    if ($jumlah > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<tr>';
+            echo '<td>' . $no++ . '</td>';
+            echo '<td>' . $row['kategori'] . '</td>';
+            echo '</tr>';
+        }
     }
-}
 
-echo '</table>';
+    echo '</table>';
 
-?>
+    ?>
+
+</div>
