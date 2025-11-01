47b3b8e08561490224e842d3912541f0b425f00b
Littlekenzy <152693956+LittleKenzy@users.noreply.github.com>
2025-11-01 19:34:50 +0700

    update

---
+++ KELAS-X-RPL/VIDEO/Php/EPS_1-52/restoran/admin/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin page | Aplikasi Restoran SMK</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <h2>Restoran</h2>
+            </div>
+
+            <div class="col-md-9">
+                <div class="float-end mt-4">Logout</div>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-md-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item"><a href="#" class="nav-link">Kategori</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Menu</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Pelanggan</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Order</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Order Detail</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">User</a></li>
+                </ul>
+            </div>
+            <div class="col-md-9">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col">
+            <p class="text-center">Copyright &copy; 2025</p>
+        </div>
+    </div>
+</body>
+
+</html>
