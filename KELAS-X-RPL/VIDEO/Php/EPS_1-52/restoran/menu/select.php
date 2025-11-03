c4dc6f5f713d7f917d14a9c4ffbe4b2bb0ea866f
Littlekenzy <152693956+LittleKenzy@users.noreply.github.com>
2025-11-03 18:21:47 +0700

    update

--- KELAS-X-RPL/VIDEO/Php/EPS_1-52/restoran/menu/select.php
+++ KELAS-X-RPL/VIDEO/Php/EPS_1-52/restoran/menu/select.php
@@ -1,1 +1,95 @@
-<h1>Select menu</h1>
+<div class="float-start" style="margin-right: 4rem;">
+    <a href="?f=menu&m=insert" class="btn btn-primary" role="button">Tambah Data</a>
+</div>
+<h1>Menu</h1>
+
+<?php 
+
+if (isset($_POST['opsi'])) {
+    $opsi = $_POST['opsi'];
+
+    $where = "WHERE idkategori = $opsi";
+} else {
+    $opsi = 0;
+    $where = "";
+}
+
+?>
+
+<div class="mt-4 mb-4">
+    <?php
+    $row = $db->getAll("SELECT * FROM tblkategori ORDER BY kategori ASC");
+    ?>
+
+
+    <form action="" method="post">
+        <select name="opsi" id="" onchange="this.form.submit()">
+            <?php foreach ($row as $r): ?>
+                <option <?php if($r['idkategori']==$opsi) echo 'selected'; ?> value="<?php echo $r['idkategori']; ?>"><?php echo $r['kategori']; ?></option>
+            <?php endforeach; ?>
+        </select>
+    </form>
+</div>
+
+<?php
+require_once '../dbcontroller.php';
+$db = new DB;
+
+
+
+$jumlah_data = $db->rowCount("SELECT idmenu FROM tblmenu");
+$banyak = 4;
+
+$halaman = ceil($jumlah_data / $banyak);
+
+if (isset($_GET['p'])) {
+    $p = $_GET['p'];
+    $mulai = ($p * $banyak) - $banyak;
+} else {
+    $mulai = 0;
+}
+
+$sql = "SELECT * FROM tblmenu $where ORDER BY menu ASC LIMIT $mulai,$banyak";
+$row = $db->getAll($sql);
+
+$no = 1 + $mulai;
+
+
+?>
+
+
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Menu</th>
+            <th>Delete</th>
+            <th>Update</th>
+        </tr>
+
+    </thead>
+    <tbody>
+        <?php if(!empty($row)) { ?>
+        <?php
+        foreach ($row as $r):
+            ?>
+            <tr>
+                <td><?php echo $no++; ?></td>
+                <td><?php echo $r['menu']; ?></td>
+                <td><a href="?f=menu&m=delete&id=<?php echo $r['idmenu']; ?>">Delete</a></td>
+                <td><a href="?f=menu&m=update&id=<?php echo $r['idmenu']; ?>">Update</a></td>
+            </tr>
+        <?php endforeach ?>
+        <?php } ?>
+    </tbody>
+</table>
+
+<?php
+
+for ($i = 1; $i < $halaman; $i++) {
+    echo '<a href="?f=menu&m=select&p=' . $i . '">' . $i . '</a>';
+    echo '&nbsp;&nbsp;&nbsp;';
+}
+
+
+?>
