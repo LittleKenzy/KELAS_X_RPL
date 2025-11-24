a0783a033d31c452d1ba1e2fd34d5e6260b77854
Littlekenzy <152693956+LittleKenzy@users.noreply.github.com>
2025-11-24 19:28:48 +0700

    update

--- KELAS-X-RPL/VIDEO/Php/EPS_1-52/restoran/order/select.php
+++ KELAS-X-RPL/VIDEO/Php/EPS_1-52/restoran/order/select.php
@@ -1,1 +1,68 @@
-<h1>Select order</h1>
+<?php
+$jumlah_data = $db->rowCount("SELECT idorder FROM vorder");
+$banyak = 2;
+
+$halaman = ceil($jumlah_data / $banyak);
+
+if (isset($_GET['p'])) {
+    $p = $_GET['p'];
+    $mulai = ($p * $banyak) - $banyak;
+} else {
+    $mulai = 0;
+}
+
+$sql = "SELECT * FROM vorder ORDER BY status DESC LIMIT $mulai, $banyak";
+$row = $db->getAll($sql);
+
+$no = 1 + $mulai;
+
+
+?>
+<h1>Order Pembelian</h1>
+
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Pelanggan</th>
+            <th>Tanggal</th>
+            <th>Total</th>
+            <th>Bayar</th>
+        </tr>
+
+    </thead>
+    <tbody>
+        <?php if (!empty($row)) { ?>
+            <?php
+            foreach ($row as $r):
+                ?>
+                <?php
+
+                if ($r['status'] == 0) {
+                    $status = '<td><a href="?f=order&m=bayar&id=' . $r['idorder'] . '">Bayar</a></td>';
+                } else {
+                    $status = '<td>Lunas</td>';
+                }
+
+                ?>
+                <tr>
+                    <td><?php echo $no++; ?></td>
+                    <td><?php echo $r['pelanggan']; ?></td>
+                    <td><?php echo $r['tglorder']; ?></td>
+                    <td><?php echo $r['total']; ?></td>
+                    <?php echo $status; ?>
+                </tr>
+            <?php endforeach ?>
+        <?php } ?>
+    </tbody>
+</table>
+
+<?php
+
+for ($i = 1; $i <= $halaman; $i++) {
+    echo '<a href="?f=order&m=select&p=' . $i . '">' . $i . '</a>';
+    echo '&nbsp;&nbsp;&nbsp;';
+}
+
+
+?>
