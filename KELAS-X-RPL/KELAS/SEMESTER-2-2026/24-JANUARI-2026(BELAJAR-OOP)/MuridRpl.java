749371adc0dea5f1d36bffefa1e1c884b5f10b6c
Littlekenzy <152693956+LittleKenzy@users.noreply.github.com>
2026-02-24 02:32:25 -0500

    update

---
+++ KELAS-X-RPL/KELAS/SEMESTER-2-2026/24-JANUARI-2026(BELAJAR-OOP)/MuridRpl.java
@@ -0,0 +1,22 @@
+public class MuridRpl {
+    String nama="Hariyanto";
+    int absen=10;
+    int usia=19;
+    String hobi="Main Game";
+   
+    void DataSiswa() {
+        System.out.println("Nama Saya adalah : "+nama);
+        System.out.println("Usia Saya adalah : "+usia);
+        System.out.println("No absen Saya : "+absen);
+        System.out.println("Hobi Saya adalah : "+hobi);
+    }
+    
+    int p=20;
+    int l=10;
+    int MenghitungPersegi() {
+        
+        int luas=p*l;
+        return luas;
+        
+    }
+}
---
+++ KELAS-X-RPL/KELAS/SEMESTER-2-2026/24-JANUARI-2026(BELAJAR-OOP)/SepuluhRPL.java
@@ -0,0 +1,22 @@
+public class SepuluhRPL {
+
+    
+    public static void main(String[] args) {
+        MuridRpl objek= new MuridRpl();
+        System.out.println(objek.nama);
+        System.out.println("Nama Anda adalah: "+ objek);
+        objek.nama="JONI";
+        System.out.println(objek.nama);
+        System.out.println(objek.absen=20);
+        System.out.println(objek.hobi);
+        System.out.println("");
+        System.out.println("");
+        
+        objek.DataSiswa();
+        
+        int Tampilanluas=objek.MenghitungPersegi();
+        System.out.println("Luas Persegi adalah "+Tampilanluas);
+        System.out.println(objek.MenghitungPersegi());
+    }
+    
+}
